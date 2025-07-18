7dd5833aa9872af8eddcb9516638ff7233602afb
dexSamir <samirah-bp215@code.edu.az>
2025-07-18 21:01:07 +0400

    feat: chessboard

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Chessboard from "./components/Chessboard";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div id="app">
+        <Chessboard />
+    </div>
+  );
+}
+
+export default App;
